5136df0b7a92aa734b9d87188786a04c9ef265af
varkxel <89874295+varkxel@users.noreply.github.com>
2022-01-14 22:13:40 +0000

    Render opaque + transparent

--- Assets/Renderer/CameraRenderer.cs
+++ Assets/Renderer/CameraRenderer.cs
@@ -9,6 +9,9 @@ namespace Mechxel
 		public ScriptableRenderContext context { get; private set; }
 		public Camera camera { get; private set; }
 		
+		// Constants
+		public static readonly ShaderTagId TagID_Unlit = new ShaderTagId("SRPDefaultUnlit");
+		
 		// Context variables
 		public CommandBuffer commandBuffer { get; private set; }
 		public CullingResults cullingResults { get; private set; }
@@ -28,7 +31,7 @@ namespace Mechxel
 			{
 				name = $"Render Camera \"{camera.name}\""
 			};
-			cullingResults = default(CullingResults);
+			cullingResults = default;
 		}
 		
 		public void Render()
@@ -63,7 +66,25 @@ namespace Mechxel
 		
 		private void DrawVisibleGeometry()
 		{
+			// Set to draw opaque
+			SortingSettings sortSettings = new SortingSettings(camera)
+			{
+				criteria = SortingCriteria.CommonOpaque
+			};
+			DrawingSettings   drawSettings   = new DrawingSettings(TagID_Unlit, sortSettings);
+			FilteringSettings filterSettings = new FilteringSettings(RenderQueueRange.opaque);
+			
+			// Draw opaque
+			context.DrawRenderers(cullingResults, ref drawSettings, ref filterSettings);
 			context.DrawSkybox(camera);
+			
+			// Set to draw transparent
+			sortSettings.criteria = SortingCriteria.CommonTransparent;
+			drawSettings.sortingSettings = sortSettings;
+			filterSettings.renderQueueRange = RenderQueueRange.transparent;
+			
+			// Draw transparent
+			context.DrawRenderers(cullingResults, ref drawSettings, ref filterSettings);
 		}
 		
 		private void Submit()
