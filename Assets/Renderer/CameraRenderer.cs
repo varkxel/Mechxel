cdee2f01d51d35e8b240e05d8986559ba32bbdb4
varkxel <89874295+varkxel@users.noreply.github.com>
2022-01-20 02:46:57 +0000

    idk

--- Assets/Renderer/CameraRenderer.cs
+++ Assets/Renderer/CameraRenderer.cs
@@ -29,7 +29,7 @@ namespace Mechxel.Renderer
 			
 			commandBuffer = new CommandBuffer
 			{
-				name = $"\"{camera.name}\" Renderer"
+				name = camera.name
 			};
 			cullingResults = default;
 		}
